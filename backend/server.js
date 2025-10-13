2690c7afbdbece4244648f8e1a2cfc8df161b2b4
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-10-13 15:02:26 +0545

    feat: set up the routes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,22 @@ import express from "express"
 const app = express();
 
 app.get("/api/notes", (req,res)=>{
-    res.send("you got 5 notes")}
+    res.send("you got 100 notes")}
     
 )
+app.post("/api/notes", (req,res)=>{
+    res.status(201).json({message: "post created successfully"})
+
+})
+app.put("/api/notes/:id", (req,res)=>{
+    res.status(200).json({message: "post updated successfully"})
+
+})
+app.delete("/api/notes/:id", (req,res)=>{
+    res.status(200).json({message: "post deleted successfully"})
+
+})
+
 
 app.listen(5001, ()=>{
     console.log("Server started on PORT: 5001")
